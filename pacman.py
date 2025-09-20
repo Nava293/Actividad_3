af2459f0d634b4af1139707b6df864f53bf666b5
fcald17 <a01669550@tec.mx>
2025-09-19 21:59:11 -0600

    Comentarios del programa y los cambios

--- pacman.py
+++ pacman.py
@@ -1,20 +1,26 @@
-from random import choice
+#Importar todos los elementos de la biblioteca gráfica 'turtle'
 from turtle import *
+##Importar piso y vectores para movimiento de la biblioteca de 'freegames'
 from freegames import floor, vector
+#Importar la biblioteca 'math' para hacer los cálculos de la distancia
 import math
 
+#Se asigna el marcador inicial en cero
 state = {'score': 0}
+#Se crean los pasillos por donde se puede pasar
 path = Turtle(visible=False)
 writer = Turtle(visible=False)
+#Se genera la dirección y posición inicial para pacman
 aim = vector(5, 0)
 pacman = vector(-40, -80)
+#Se genera el arreglo que contiene los 4 fantasmas
 ghosts = [
     [vector(-180, 160), vector(5, 0)],
     [vector(-180, -160), vector(0, 5)],
     [vector(100, 160), vector(0, -5)],
     [vector(100, -160), vector(-5, 0)],
 ]
-# fmt: off
+# Arreglo en el que se basa el mapa
 tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
@@ -37,9 +43,8 @@ tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 ]
-# fmt: on
 
-
+#Función donde se genera el camino que pacman puede seguir
 def square(x, y):
     path.up()
     path.goto(x, y)
@@ -52,14 +57,14 @@ def square(x, y):
 
     path.end_fill()
 
-
+#Función en la que se transforman los vectores de pacman y los fantasmas en coordenadas para el arreglo del mapa, necesario para poder ubicar ambos
 def offset(point):
     x = (floor(point.x, 20) + 200) / 20
     y = (180 - floor(point.y, 20)) / 20
     index = int(x + y * 20)
     return index
 
-
+#Función que se asegura si pacman y los fantasmas se encuentran dentro de los caminos
 def valid(point):
     index = offset(point)
 
@@ -73,7 +78,7 @@ def valid(point):
 
     return point.x % 20 == 0 or point.y % 20 == 0
 
-
+#Función para generar el mundo en la pantalla visual y colorearlo, azul para el camino y negro para las paredes o zonas fuera de alcance
 def world():
     bgcolor('black')
     path.color('blue')
@@ -91,7 +96,7 @@ def world():
                 path.goto(x + 10, y + 10)
                 path.dot(2, 'white')
 
-
+#Función para determinar el movimiento de pacman y los fantasmas, si el siguiente paso es válido y en el caso de los fantasmas, el camino más corto a pacman
 def move():
     writer.undo()
     writer.write(state['score'])
@@ -128,20 +133,25 @@ def move():
                 vector(0, 5),
                 vector(0, -5),
             ]
+            #Genera variables para guardar el mejor camino a Pacman
             best_plan = course
             best_distance = 100000
             
+            #Se hace un for para crear el mejor camino para cada fantasma dentro del arreglo
             for plan in options:
+                #Se calcula la mejor distancia de la posición de Pacman a la del fantasma
                 dx = (point + plan).x - pacman.x
                 dy = (point + plan).y - pacman.y
                 distance = math.sqrt(dx**2 + dy**2)
                 
+                #Si el camino más corto es viable, se guarda como el camino o plan
                 if distance < best_distance and valid(point + plan):
                     best_distance = distance
                     best_plan = plan
-            
+            #El fantasma cambia su dirección de camino a la distancia más corta validada
             course.x = best_plan.x
             course.y = best_plan.y
+            #Se mueve el fantasma en la nueva dirección
             point.move(course)
 
         up()
@@ -156,19 +166,21 @@ def move():
 
     ontimer(move, 30)
 
-
+#Cambio de dirección de pacman, usado para cuando se presiona una tecla asignada a una dirección
 def change(x, y):
     if valid(pacman + vector(x, y)):
         aim.x = x
         aim.y = y
 
-
+#Se genera la pantalla gráfica
 setup(420, 420, 370, 0)
+#Se oculta la tortuga que genera los gráficos
 hideturtle()
 tracer(False)
 writer.goto(160, 160)
 writer.color('white')
 writer.write(state['score'])
+#Se espera el uso de una tecla y se le asigna su debida dirección
 listen()
 onkey(lambda: change(5, 0), 'Right')
 onkey(lambda: change(-5, 0), 'Left')
